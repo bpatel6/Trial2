94d50bfe09ae091fc24eb8a3f22a7e619e0d9712
Bhavik Patel <bpatel6@uiowa.edu>
2019-09-11 13:07:15 -0500

    updating the reader file

--- src/Reader.java
+++ src/Reader.java
@@ -5,16 +5,63 @@ public class Reader {
     // this array is for storing the Ebooks in the order they were added.
     // We require that you use it for that purpose.
     private final Ebook[] library;
-
+    int size;
+    private final int charperpage;
     /* add more instance variables that you need
     */
 
     Reader(int capacity, int charactersPerPage) {
-        library = null;
+        library = new Ebook[capacity];
+        size = 0;
+        charperpage = charactersPerPage;
     }
 
     /* Copy/paste into here the methods listed in the PDF
        and then implement them
      */
 
+    public void addBook(URL url) {
+        this.library[this.size] = new Ebook(SystemTools.download(url));
+        size += 1;
+    }
+
+    public void addBook(String text) {
+        this.library[this.size] = new Ebook(text);
+        size += 1;
+    }
+
+    public void deleteBook(int bid) throws IndexOutOfBoundsException {
+        if (bid < 0 || bid >= size){
+            throw new IndexOutOfBoundsException("Invalid bid: " + bid);
+        }
+        Ebook temp = library[bid];
+        for (int j = bid; j < size-1; j++){
+            library[j] = library[j+1];
+        }
+        library[size-1] = null;
+        size--;
+    }
+
+    public int numBooks(){
+        return size;
+    }
+
+    public void printTitles(){
+        for (int i = 0; i < size; i++){
+            library[i].toString();
+        }
+    }
+
+    public void openBook(int bid) throws IndexOutOfBoundsException{
+        if (bid < 0 || bid >= size){
+            throw new IndexOutOfBoundsException("Invalid bid: " + bid);
+        }
+    }
+
+    public void turnToPage(int page){
+
+    }
+    public String readPage(){
+
+    }
 }
